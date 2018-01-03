ad89873d426300dc8c9dbcc0fbe4d37cc0d02c8f
Chio <2284957575@qq.com>
2018-01-03 15:31:48 +0800

    容器系列

--- src/main/java/com/ioc/IocOfContainer.java
+++ src/main/java/com/ioc/IocOfContainer.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.Constants;
 import org.springframework.core.env.EnvironmentCapable;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.context.support.AbstractRefreshableWebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
 /**
  * @Author ZGang
@@ -16,38 +21,18 @@ import org.springframework.core.env.EnvironmentCapable;
 public class IocOfContainer {
 
     /**
-     * 查看BeanFactory 接口源码
-     *  BeanFactory接口是IOC容器基础规范, 其定义了容器的基本功能方法
-     */
-    public void seeBeanFactoryInterface(){
-
-        BeanFactory beanFactory = null;
-
-        beanFactory.containsBean("a");//这个bean是不是在容器内
-
-        beanFactory.getAliases("");//获得Bean别名
-
-        beanFactory.getBean("beanName");//通过名称获得类对象
-
-        beanFactory.getBean(IocOfContainer.class);//通过类类型来获得类对象
-
-        beanFactory.getType("");//获得类类型
-
-    }
-
-    /**
      * ApplicationContext 其是Spring IOC的最高形态
      */
     public void seeApplicationContextInterFace(){
         ApplicationContext context = null;
 
-    }
+        ConfigurableApplicationContext configurableApplicationContext;
 
-    public void seeXmlBeanFactoryClass(){
-        XmlBeanFactory xmlBeanFactory = null;//在Spring3.1之后   建议使用 {@link XmlBeanDefinitionReader  或者DefaultListableBeanFactory
-        //从XmlBeanDefinitionReader  看如何从Xml中获得Bean的定义 以及Bean的依赖关系
-        XmlBeanDefinitionReader xmlBeanDefinitionReader = null;
+        AbstractRefreshableWebApplicationContext abstractRefreshableWebApplicationContext;
 
+        XmlWebApplicationContext xmlWebApplicationContext = null;
+
+        AnnotationConfigWebApplicationContext annotationConfigWebApplicationContext = null;
 
     }
 }
