f243b152129c15d1e135251c873fef1b69674a32
Weirdo <525340603@qq.com>
2018-01-10 17:04:42 +0800

    Spring mvc dispatcherServlet报错改正 新增 spring mvc 适配器类 新增 spring mvc 拦截器实现类

---
+++ src/main/java/com/mvc/HandlerAdapter.java
@@ -0,0 +1,10 @@
+package com.mvc;
+
+/**
+ * springmvc 处理适配器
+ */
+public class HandlerAdapter {
+    org.springframework.web.servlet.HandlerAdapter handlerAdapter ;
+    //HandlerAdapter#.supports;判断处理适配器是否支持该handler;
+    //HandlerAdapter#.handle;通过适配器执行对应的方法
+}
